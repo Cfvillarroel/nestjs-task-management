53a433cb2be5d5fd2388c3754cf9fc43f76599a5
Carlos Villarroel <cavillarroel11@gmail.com>
2019-10-17 15:43:12 -0300

    Working

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import { TasksService } from './tasks/tasks.service';
+import { TasksController } from './tasks/tasks.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TasksModule],
+  controllers: [TasksController],
+  providers: [TasksService],
 })
-export class AppModule {}
+export class AppModule { }
