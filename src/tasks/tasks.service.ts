36255f8caae42bc54e3a0e360bf5601c7ea8f999
Carlos Villarroel <cavillarroel11@gmail.com>
2019-10-24 09:03:13 -0300

    Refactoring for use of DataBase

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,12 +1,10 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Task, TaskStatus } from './task.model';
-import * as uuid from 'uuid/v1';
 import { CreateTaskDto } from './dto/create-task-dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
-    private tasks: Task[] = [];
+    /* private tasks: Task[] = [];
 
     getAllTasks(): Task[] {
         return this.tasks;
@@ -31,7 +29,7 @@ export class TasksService {
        const found = this.tasks.find(task => task.id === id);
 
        if (!found) {
-           throw new NotFoundException(`Task with ID "${id}" not found`);
+         //  throw new NotFoundException();
        }
 
        return found;
@@ -59,6 +57,6 @@ export class TasksService {
         const task = this.getTaskById(id);
         task.status = status;
         return task;
-    }
+    } */
 
 }
