52d8a0fbf64acdb6fccd4d1c21f83241449e4087
Carlos Villarroel <cavillarroel11@gmail.com>
2019-10-25 15:08:56 -0300

    Custom Decorator @Get User

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Post, Body, ValidationPipe, UseGuards, Req } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
-import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -18,10 +17,4 @@ export class AuthController {
         return this.authService.signIn(authCredentialsDto);
     }
 
-    @Post('/test')
-    @UseGuards(AuthGuard())
-    test(@Req() req) {
-        // tslint:disable-next-line: no-console
-        console.log(req);
-    }
 }
