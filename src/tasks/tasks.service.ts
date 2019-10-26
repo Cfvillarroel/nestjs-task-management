68b962fbd2ef1e5903ef0e6bc7614d33a162e4d4
Carlos Villarroel <cavillarroel11@gmail.com>
2019-10-26 09:03:14 -0300

    Update and Delete for User

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -34,8 +34,8 @@ export class TasksService {
 
     }
 
-    async deleteTask(id: number): Promise<void> {
-        const result = await this.taskRepository.delete(id);
+    async deleteTask(id: number, user: User): Promise<void> {
+        const result = await this.taskRepository.delete({id, userId: user.id});
 
         if (result.affected === 0) {
             throw new NotFoundException(`Task with ID "${id}" not found`);
@@ -43,11 +43,11 @@ export class TasksService {
 
     }
 
-    /* async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
-        const task = await this.getTaskById(id);
+    async updateTaskStatus(id: number, status: TaskStatus, user: User): Promise<Task> {
+        const task = await this.getTaskById(id, user);
         task.status = status;
         await task.save();
         return task;
-    } */
+    }
 
 }
