8857e31baf1a62e408e2a3c5d889e2acc1207261
Carlos <cavillarroel11@gmail.com>
2019-10-28 12:16:33 -0300

    Run with Frontend

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,14 @@ import { AppModule } from './app.module';
 import * as config from 'config';
 
 async function bootstrap() {
-  const serverConfig = config.get('server');
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
+
+  const serverConfig = config.get('server');
+
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
 
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
